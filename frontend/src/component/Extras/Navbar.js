e2f5b51f46befcd31c307caa756b9c75debef9d6
Dhruvermafz <vermadhruv09112002@gmail.com>
2024-10-15 12:29:20 +0530

    products seeded

--- frontend/src/component/Extras/Navbar.js
+++ frontend/src/component/Extras/Navbar.js
@@ -6,13 +6,13 @@ import "../../styles/navbar.css";
 const Navbar = () => {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <div class="container">
-          <a class="navbar-brand" href="/">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div className="container">
+          <a className="navbar-brand" href="/">
             <img src={logo} alt="" />
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -20,18 +20,18 @@ const Navbar = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav m-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to="/">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav m-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
                 </Link>
               </li>
-              {/* <li class="nav-item">
+              {/* <li className="nav-item">
                 <Link
-                  class="nav-link active"
+                  className="nav-link active"
                   aria-current="page"
                   to={"/product"}
                 >
@@ -39,10 +39,10 @@ const Navbar = () => {
                 </Link>
               </li> */}
 
-              <li class="nav-item dropdown">
+              <li className="nav-item dropdown">
                 <Link
                   to={"/products"}
-                  class="nav-link dropdown-toggle"
+                  className="nav-link dropdown-toggle"
                   href="#"
                   id="navbarDropdown"
                   role="button"
@@ -51,69 +51,77 @@ const Navbar = () => {
                 >
                   Product
                 </Link>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                   <li>
-                    <Link class="dropdown-item" href="#">
+                    <Link className="dropdown-item" href="#">
                       Attendance and Access Control Systems
                     </Link>
                   </li>
                   <li>
-                    <Link class="dropdown-item" href="#">
+                    <Link className="dropdown-item" href="#">
                       Door Locks and Controllers
                     </Link>
                   </li>
                   <li>
-                    <Link class="dropdown-item" href="#">
+                    <Link className="dropdown-item" href="#">
                       Barcode Scanners
                     </Link>
                   </li>
                   <li>
-                    <Link class="dropdown-item" href="#">
+                    <Link className="dropdown-item" href="#">
                       Accessories
                     </Link>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="#">
+                    <a className="dropdown-item" href="#">
                       Switches for Access Control
                     </a>
                   </li>
                   <li>
-                    <Link class="dropdown-item" href="#">
+                    <Link className="dropdown-item" href="#">
                       Readers
                     </Link>
                   </li>
                 </ul>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link
-                  class="nav-link active"
+                  className="nav-link active"
                   aria-current="page"
                   to="/products"
                 >
                   Our Store
                 </Link>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to="/aboutus">
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/aboutus"
+                >
                   About us
                 </Link>
               </li>
 
-              <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to="/contact">
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/contact"
+                >
                   Contact us
                 </Link>
               </li>
             </ul>
-            {/* <div class="d-flex btn-group ">
+            {/* <div className="d-flex btn-group ">
               <Link>
                 <i
-                  className="bx bxs-cart"
+                  classNameName="bx bxs-cart"
                   style={{ fontSize: "24px", color: "#000" }}
                 ></i>
               </Link>
             </div> */}
-            <div class="d-flex btn-group ms-2">
+            <div className="d-flex btn-group ms-2">
               <button className=" btn1">
                 <Link to={"/signin"}>Login</Link>
               </button>
