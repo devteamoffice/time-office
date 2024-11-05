fdc500ca7d82214235298735bbee96442c9d2afd
jojovikas <vikasfrontenddeveloper007@gmail.com>
2024-11-05 16:21:30 +0530

    newpush

--- Frontend update/src/component/HomePage/SliderBrand.js
+++ Frontend update/src/component/HomePage/SliderBrand.js
@@ -6,9 +6,77 @@ import s2 from "../../assets/Image/Slider1/image 20.png";
 import s3 from "../../assets/Image/Slider1/image 21.png";
 import s4 from "../../assets/Image/Slider1/image 22.png";
 import s5 from "../../assets/Image/Slider1/image 23.png";
+import n1 from "../../assets/Image/Slider1/1 .png";
+import n2 from "../../assets/Image/Slider1/2.jpg";
+import n3 from "../../assets/Image/Slider1/3.jpg";
+import n4 from "../../assets/Image/Slider1/4.png";
+import n5 from "../../assets/Image/Slider1/5.jpg";
+import n6 from "../../assets/Image/Slider1/6.jpg";
+import n7 from "../../assets/Image/Slider1/7.jpg";
+import n8 from "../../assets/Image/Slider1/8.jpg";
+import n9 from "../../assets/Image/Slider1/9.jpg";
+
+const products = [
+  {
+    image: n1,
+    title: "Z20R V2 Glass Door Lock",
+  },
+  {
+    image: n2,
+    title: "Electromagnetic Lock ",
+  },
+  {
+    image: n3,
+    title: "Remote Kit",
+  },
+  {
+    image: n4,
+    title: "Drop Bolt Lock",
+  },
+  {
+    image: n5,
+    title: "U Bracket For EM Lock",
+  },
+  {
+    image: n6,
+    title: "Thin Card",
+  },
+  {
+    image: n7,
+    title: "AI Face Identification",
+  },
+  {
+    image: n8,
+    title: "Emergency Glass Break Switch",
+  },
+  {
+    image: n9,
+    title: "Drop Bolt Lock Fully Glass",
+  },
+  {
+    image: s1,
+    title: "Door Locks & Brackets",
+  },
+  {
+    image: s2,
+    title: "Access Control System",
+  },
+  {
+    image: s3,
+    title: "Biometric Access Control",
+  },
+  {
+    image: s4,
+    title: "Barcode Scanner",
+  },
+  {
+    image: s5,
+    title: "Computerized Time Attendance Device",
+  },
+];
 
 const SliderBrand = () => {
-  var settings = {
+  const settings = {
     dots: true,
     infinite: true,
     speed: 500,
@@ -44,6 +112,7 @@ const SliderBrand = () => {
       },
     ],
   };
+
   return (
     <div className="container">
       <div className="row">
@@ -55,162 +124,33 @@ const SliderBrand = () => {
         <div className="col-md-12">
           <div className="slider-container productScroll">
             <Slider {...settings}>
-              <div className="mr-2">
-                <div className="slider-inner">
-                  <img src={s1} alt="" />
-                  <div class="d-flex align-items-center gap-2 mb-1">
-                    <ul class="d-flex text-warning m-0 fs-20 list-unstyled">
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star-half"></i>
-                      </li>
-                    </ul>
+              {products.map((product, index) => (
+                <div className="mr-2" key={index}>
+                  <div className="slider-inner">
+                    <img src={product.image} alt={product.title} />
+                    <div className="d-flex align-items-center gap-2 mb-1">
+                      <ul className="d-flex text-warning m-0 fs-20 list-unstyled">
+                        <li>
+                          <i className="bx bxs-star"></i>
+                        </li>
+                        <li>
+                          <i className="bx bxs-star"></i>
+                        </li>
+                        <li>
+                          <i className="bx bxs-star"></i>
+                        </li>
+                        <li>
+                          <i className="bx bxs-star"></i>
+                        </li>
+                        <li>
+                          <i className="bx bxs-star-half"></i>
+                        </li>
+                      </ul>
+                    </div>
+                    <h4 style={{ textAlign: "center" }}>{product.title}</h4>
                   </div>
-                  <h4>Door Locks & Brackets</h4>
                 </div>
-              </div>
-              <div className="mr-2">
-                <div className="slider-inner">
-                  <img src={s2} alt="" />
-                  <div class="d-flex align-items-center gap-2 mb-1">
-                    <ul class="d-flex text-warning m-0 fs-20 list-unstyled">
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star-half"></i>
-                      </li>
-                    </ul>
-                  </div>
-                  <h4>Access Control System</h4>
-                </div>
-              </div>
-
-              <div className="mr-2">
-                <div className="slider-inner">
-                  <img src={s3} alt="" />
-                  <div class="d-flex align-items-center gap-2 mb-1">
-                    <ul class="d-flex text-warning m-0 fs-20 list-unstyled">
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star-half"></i>
-                      </li>
-                    </ul>
-                  </div>
-                  <h4>Biometric Access Controll </h4>
-                </div>
-              </div>
-
-              <div className="mr-2">
-                <div className="slider-inner">
-                  <img src={s4} alt="" />
-                  <div class="d-flex align-items-center gap-2 mb-1">
-                    <ul class="d-flex text-warning m-0 fs-20 list-unstyled">
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star-half"></i>
-                      </li>
-                    </ul>
-                  </div>
-                  <h4>Barcode Scanner</h4>
-                </div>
-              </div>
-
-              <div className="mr-2">
-                <div className="slider-inner">
-                  <img src={s5} alt="" />
-                  <div class="d-flex align-items-center gap-2 mb-1">
-                    <ul class="d-flex text-warning m-0 fs-20 list-unstyled">
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star-half"></i>
-                      </li>
-                    </ul>
-                  </div>
-                  <h4 style={{ textAlign: "center" }}>
-                    Computerized Time Attendance Device
-                  </h4>
-                </div>
-              </div>
-
-              <div className="mr-2">
-                <div className="slider-inner">
-                  <img src={s1} alt="" />
-                  <div class="d-flex align-items-center gap-2 mb-1">
-                    <ul class="d-flex text-warning m-0 fs-20 list-unstyled">
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star"></i>
-                      </li>
-                      <li>
-                        <i class="bx bxs-star-half"></i>
-                      </li>
-                    </ul>
-                  </div>
-                  <h4>Door Locks & Brackets</h4>
-                </div>
-              </div>
+              ))}
             </Slider>
           </div>
         </div>
